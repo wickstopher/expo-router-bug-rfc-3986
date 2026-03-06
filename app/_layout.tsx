f19b8bddb5f3a6d2c3f9fb439c66946069424122
Christopher R. Wicks <cwicks@visualdx.com>
2026-03-06 15:10:03 -0500

    Add route and update README

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="some_route" options={{ title: 'Some Route' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
